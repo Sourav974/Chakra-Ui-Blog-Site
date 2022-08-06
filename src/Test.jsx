6b17ffbb1bcad895bdf7617582258f7e815f8735
Sourav <sourav@192.168.1.6>
2022-08-06 13:01:27 +0530

    First Commit

---
+++ src/Test.jsx
@@ -0,0 +1,15 @@
+import {Box, Heading, Container} from '@chakra-ui/react'
+
+const Test = () => {
+    <Box mr="6">
+    <Heading as="h1" size="2xl">
+    <Box fontWeight="black">
+        Hello
+    </Box>
+    </Heading>
+    </Box>
+
+}
+
+
+export default Test;
